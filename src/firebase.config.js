bb76e9c26b2b5851895ec7a20be38ee404aeff60
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-09-02 14:26:38 +0530

    updated

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,16 +1,17 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+// require('dotenv').config();
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    apiKey: "AIzaSyDUFna-6b-B7aP2aRdNW56GlUMHJnhoWl0",
     authDomain: "bit-canteen.firebaseapp.com",
     databaseURL: "https://bit-canteen-default-rtdb.firebaseio.com",
     projectId: "bit-canteen",
     storageBucket: "bit-canteen.appspot.com",
     messagingSenderId: "119692725179",
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    appId: "1:119692725179:web:3e05019e52ed8cc909929e",
+    measurementId: "G-7X5HW7N0ZY"
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
